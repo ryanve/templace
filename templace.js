44c452097baae68b557cfb6469804de75f6cbb99
Ryan Van Etten <ryanve@gmail.com>
2021-11-11 22:57:14 -0500

    legit loop

--- templace.js
+++ templace.js
@@ -1,25 +1,29 @@
 /** @preserve npm.im/templace */
 !function(that) {
-  function noop() {}
-  function loop() {
+  function api() {
+    var sup
     var plate
     var clone
-    do {
-      plate = document[next](place)
-      if (!plate) break
-      if (!("content" in plate)) break
+    var l = live.length
+    var i = 0
+    var d = 0
+    while (i < l) {
+      plate = live[i++]
+      sup = sup || "content" in plate
+      if (!sup) break
+      if (!plate.hasAttribute(data)) continue
       clone = plate.content.cloneNode(true)
       plate.parentNode.insertBefore(clone, plate)
       plate.removeAttribute(data)
-    } while (plate)
+      d++
+    }
+    return d
   }
   var name = "templace"
-  var dom = typeof document != "undefined"
-  var next = "querySelector"
   var data = "data-templace"
-  var place = "template[data-templace]"
-  var active = dom && next in document
-  var api = active ? loop : noop
+  var tag = "getElementsByTagName"
+  var ufo = typeof document == "undefined"
+  var live = ufo || document[tag]("template")
   var common = typeof module != "undefined"
   common && module.exports
     ? module.exports = api
